360190268af8249b929ffdf02312885f3e0aa125
kaauthama <kaauthama@gmail.com>
2022-02-15 19:26:08 -0300

    criamos variaveis

---
+++ var.js
@@ -0,0 +1,10 @@
+var numero1 = 10;
+var numero2 = 20;
+var operador = "multiplicar";
+if (operador=="somar"){
+console.log(numero1+numero2);
+}
+
+if (operador=="multiplicar"){
+    console.log(numero1*numero2);
+}
