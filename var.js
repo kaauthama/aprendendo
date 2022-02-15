8e31654e52621553be75740a9ba6f25c442b00c3
kaauthama <kaauthama@gmail.com>
2022-02-15 19:49:09 -0300

    add subtracao e divisao

--- var.js
+++ var.js
@@ -1,8 +1,10 @@
 var numero1 = 10;
 var numero2 = 20;
-var operador = "multiplicar";
+var operador = "dividir";
 var soma = numero1+numero2;
 var multiplica = numero1*numero2;
+var subtrair = numero1-numero2;
+var dividir = numero1/numero2;
 
 
 
@@ -12,4 +14,11 @@ console.log(soma);
 
 if (operador=="multiplicar"){
     console.log(multiplica);
+}
+
+if(operador=="subtrair"){
+    console.log(subtrair);
+}
+if(operador=="dividir"){
+    console.log(dividir);
 }
