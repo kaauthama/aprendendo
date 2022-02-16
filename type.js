42329a3d48f4b6a0ab1d63309eed03f2b50b09af
kaauthama <kaauthama@gmail.com>
2022-02-15 23:14:09 -0300

    tipos de variaveis

---
+++ type.js
@@ -0,0 +1,89 @@
+// tipo string
+//var escola = "aluno";
+//console.log(escola);
+
+//tipo number
+//var number1 = 01;
+//console.log(number1);
+
+//tipo number inteiro
+//var numero = 10;
+//console.log(numero);
+
+
+//tipo number decimal
+//var numerodec = 3.31
+4;
+//console.log(numerodec);
+
+//tipo booleano
+//var idade = 37;
+//var verificaIdade =  idade>38;
+//console.log(verificaIdade);
+
+//tipo null
+//var cadeira = null;
+//console.log(cadeira);
+
+//tipo undefined
+//var frida;
+//console.log(frida);
+
+// tipo not a number
+//var estounervoso = NaN;
+//console.log(estounervoso);
+
+//tipo objeto
+// var carro = {
+//     motor: 1.6,
+//     cor: "azul",
+//     fabricante : "toyota",
+//     tipo: "caminhonete",
+//     pneu: "aro18"    
+// };
+
+// var pessoa = {
+//     nome: "maria",
+//     idade: "21",
+//     cor: "parda",
+//     genero: "feminino"
+// };
+
+// var paciente = {
+//     nome: "joao",
+//     idade: "58",
+//     genero: "masculino",
+//     convenio: "sus"
+// };
+
+// console.log(carro);
+// console.log(pessoa);
+// console.log(paciente);
+
+
+//tipo Array
+// var carro = ["azul","suv", "aro18","flex","toyota"];
+// var listadafeira = [ "farinha", "tapioca", "abacate", "mel"];
+// var varios = [
+//     {
+//         cor: "azul", 
+//         aro: 18,
+//         tipo: "suv"
+//     },
+//     {
+//         nome: "maria",
+//         idade: 21,
+//         cor: "parda",
+//         genero: "masculino"  
+//     },
+//     {   nome: "joao",
+//         idade: 58,
+//         genero: "masculino",
+//         convenio: "sus"
+//     }
+// ];
+// console.log (carro);
+// console.log (listadafeira);
+// console.log (varios[0]);
+
+
